e31944a2c60f77cb33c583508bedcdf98ff0ef10
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-05-07 12:06:03 -0700

    Add client and server code

---
+++ server.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <thallium.hpp>
+
+namespace tl = thallium;
+
+void hello(const tl::request& req) {
+    std::cout << "Hello World!" << std::endl;
+}
+
+int main(int argc, char** argv) {
+
+    tl::engine myEngine("tcp", THALLIUM_SERVER_MODE);
+    myEngine.define("hello", hello).disable_response();
+    std::cout << "Server running at address " << myEngine.self() << std::endl;
+
+    return 0;
+}
