f14fa7b031a5929fea9f6c79399736aca1cb8e8f
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-05-20 12:20:37 -0700

    move to the RDMA example

--- src/server.cc
+++ src/server.cc
@@ -76,12 +76,12 @@ int main(int argc, char** argv) {
     // define the RPC method   
     std::function<void(const tl::request&)> s = 
         [&engine, &do_rdma](const tl::request &req) {
-            std::cout << "scan() called" << std::endl;
             std::string buffer = "Matthieu";
             std::vector<std::pair<void*,std::size_t>> segments(1);
             segments[0].first  = (void*)(&buffer[0]);
             segments[0].second = buffer.size()+1;
             tl::bulk arrow_bulk = engine.expose(segments, tl::bulk_mode::read_only);
+            std::cout << "About to do RDMA:" << req.get_endpoint() << std::endl;
             do_rdma.on(req.get_endpoint())(arrow_bulk);
         };
     engine.define("scan", s).disable_response();
