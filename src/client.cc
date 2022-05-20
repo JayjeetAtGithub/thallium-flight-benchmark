969b53ff56da7e5d95e7ec7a867a640eed333b85
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-05-20 11:27:22 -0700

    move to the RDMA example

--- src/client.cc
+++ src/client.cc
@@ -1,18 +1,41 @@
+// #include <thallium.hpp>
+
+// namespace tl = thallium;
+
+// int main(int argc, char** argv) {
+
+//     if(argc != 2) {
+//         std::cerr << "Usage: " << argv[0] << " <address>" << std::endl;
+//         exit(0);
+//     }
+
+//     tl::engine myEngine("tcp", THALLIUM_CLIENT_MODE);
+//     tl::remote_procedure hello = myEngine.define("hello").disable_response();
+//     tl::endpoint server = myEngine.lookup(argv[1]);
+//     hello.on(server)();
+
+//     return 0;
+// }
+
+#include <iostream>
 #include <thallium.hpp>
 
 namespace tl = thallium;
 
 int main(int argc, char** argv) {
 
-    if(argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <address>" << std::endl;
-        exit(0);
-    }
+    tl::engine myEngine("tcp", MARGO_CLIENT_MODE);
+    tl::remote_procedure remote_do_rdma = myEngine.define("do_rdma").disable_response();
+    tl::endpoint server_endpoint = myEngine.lookup("tcp://127.0.0.1:1234");
 
-    tl::engine myEngine("tcp", THALLIUM_CLIENT_MODE);
-    tl::remote_procedure hello = myEngine.define("hello").disable_response();
-    tl::endpoint server = myEngine.lookup(argv[1]);
-    hello.on(server)();
+    std::string buffer = "Matthieu";
+    std::vector<std::pair<void*,std::size_t>> segments(1);
+    segments[0].first  = (void*)(&buffer[0]);
+    segments[0].second = buffer.size()+1;
+
+    tl::bulk myBulk = myEngine.expose(segments, tl::bulk_mode::read_only);
+
+    remote_do_rdma.on(server_endpoint)(myBulk);
 
     return 0;
 }
--- src/server.cc
+++ src/server.cc
@@ -1,58 +1,85 @@
+// #include <iostream>
+
+// #include "arrow/api.h"
+// #include "arrow/compute/exec/expression.h"
+// #include "arrow/dataset/api.h"
+// #include "arrow/filesystem/api.h"
+// #include "arrow/io/api.h"
+// #include "arrow/util/checked_cast.h"
+// #include "arrow/util/iterator.h"
+// #include "parquet/arrow/reader.h"
+// #include "parquet/arrow/writer.h"
+// #include <thallium.hpp>
+
+
+// arrow::Result<std::shared_ptr<arrow::Table>> Scan() {
+//     std::shared_ptr<arrow::fs::LocalFileSystem> fs =
+//         std::make_shared<arrow::fs::LocalFileSystem>();
+
+//     arrow::fs::FileSelector selector;
+//     selector.base_dir = "/mnt/cephfs/dataset";
+//     selector.recursive = true;
+
+//     ARROW_ASSIGN_OR_RAISE(std::vector<arrow::fs::FileInfo> file_infos,
+//         fs->GetFileInfo(selector));
+
+//     std::shared_ptr<arrow::dataset::ParquetFileFormat> format =
+//         std::make_shared<arrow::dataset::ParquetFileFormat>();
+
+//     arrow::dataset::FileSystemFactoryOptions options;
+//     ARROW_ASSIGN_OR_RAISE(
+//         std::shared_ptr<arrow::dataset::DatasetFactory> dataset_factory,
+//         arrow::dataset::FileSystemDatasetFactory::Make(fs, selector, format, options));
+
+//     ARROW_ASSIGN_OR_RAISE(std::shared_ptr<arrow::dataset::Dataset> dataset,
+//         dataset_factory->Finish());
+
+//     arrow::dataset::ScannerBuilder scanner_builder(dataset);
+//     ARROW_RETURN_NOT_OK(scanner_builder.UseThreads(true));
+//     ARROW_ASSIGN_OR_RAISE(std::shared_ptr<arrow::dataset::Scanner> scanner,
+//                         scanner_builder.Finish());
+
+//     ARROW_ASSIGN_OR_RAISE(std::shared_ptr<arrow::Table> table, scanner->ToTable());
+//     return table;
+// }
+
+// void hello(const thallium::request& req) {
+//     std::shared_ptr<arrow::Table> table = Scan().ValueOrDie();
+//     std::cout << table->num_rows() << std::endl;
+// }
+
+// int main(int argc, char** argv) {
+
+//     thallium::engine myEngine("tcp", THALLIUM_SERVER_MODE);
+//     myEngine.define("hello", hello).disable_response();
+//     std::cout << "Server running at address " << myEngine.self() << std::endl;
+
+//     return 0;
+// }
+
+
 #include <iostream>
-
-#include "arrow/api.h"
-#include "arrow/compute/exec/expression.h"
-#include "arrow/dataset/api.h"
-#include "arrow/filesystem/api.h"
-#include "arrow/io/api.h"
-#include "arrow/util/checked_cast.h"
-#include "arrow/util/iterator.h"
-#include "parquet/arrow/reader.h"
-#include "parquet/arrow/writer.h"
 #include <thallium.hpp>
+#include <thallium/serialization/stl/string.hpp>
 
-
-arrow::Result<std::shared_ptr<arrow::Table>> Scan() {
-    std::shared_ptr<arrow::fs::LocalFileSystem> fs =
-        std::make_shared<arrow::fs::LocalFileSystem>();
-
-    arrow::fs::FileSelector selector;
-    selector.base_dir = "/mnt/cephfs/dataset";
-    selector.recursive = true;
-
-    ARROW_ASSIGN_OR_RAISE(std::vector<arrow::fs::FileInfo> file_infos,
-        fs->GetFileInfo(selector));
-
-    std::shared_ptr<arrow::dataset::ParquetFileFormat> format =
-        std::make_shared<arrow::dataset::ParquetFileFormat>();
-
-    arrow::dataset::FileSystemFactoryOptions options;
-    ARROW_ASSIGN_OR_RAISE(
-        std::shared_ptr<arrow::dataset::DatasetFactory> dataset_factory,
-        arrow::dataset::FileSystemDatasetFactory::Make(fs, selector, format, options));
-
-    ARROW_ASSIGN_OR_RAISE(std::shared_ptr<arrow::dataset::Dataset> dataset,
-        dataset_factory->Finish());
-
-    arrow::dataset::ScannerBuilder scanner_builder(dataset);
-    ARROW_RETURN_NOT_OK(scanner_builder.UseThreads(true));
-    ARROW_ASSIGN_OR_RAISE(std::shared_ptr<arrow::dataset::Scanner> scanner,
-                        scanner_builder.Finish());
-
-    ARROW_ASSIGN_OR_RAISE(std::shared_ptr<arrow::Table> table, scanner->ToTable());
-    return table;
-}
-
-void hello(const thallium::request& req) {
-    std::shared_ptr<arrow::Table> table = Scan().ValueOrDie();
-    std::cout << table->num_rows() << std::endl;
-}
+namespace tl = thallium;
 
 int main(int argc, char** argv) {
 
-    thallium::engine myEngine("tcp", THALLIUM_SERVER_MODE);
-    myEngine.define("hello", hello).disable_response();
-    std::cout << "Server running at address " << myEngine.self() << std::endl;
+    tl::engine myEngine("tcp://127.0.0.1:1234", THALLIUM_SERVER_MODE);
 
-    return 0;
+    std::function<void(const tl::request&, tl::bulk&)> f =
+        [&myEngine](const tl::request& req, tl::bulk& b) {
+            tl::endpoint ep = req.get_endpoint();
+            std::vector<char> v(6);
+            std::vector<std::pair<void*,std::size_t>> segments(1);
+            segments[0].first  = (void*)(&v[0]);
+            segments[0].second = v.size();
+            tl::bulk local = myEngine.expose(segments, tl::bulk_mode::write_only);
+            b.on(ep) >> local;
+            std::cout << "Server received bulk: ";
+            for(auto c : v) std::cout << c;
+            std::cout << std::endl;
+        };
+    myEngine.define("do_rdma",f).disable_response();
 }
