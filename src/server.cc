f1fedcfde8b05860d7a784458153996bd57e3fa7
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-05-20 11:44:07 -0700

    move to the RDMA example

--- src/server.cc
+++ src/server.cc
@@ -69,18 +69,12 @@ int main(int argc, char** argv) {
     tl::engine engine("tcp", THALLIUM_SERVER_MODE);
     std::cout << "Server running at address " << engine.self() << std::endl;
     
-    std::function<void(const tl::request&, tl::bulk&)> f =
-        [&engine](const tl::request& req, tl::bulk& b) {
-            tl::endpoint ep = req.get_endpoint();
-            std::vector<char> v(6);
-            std::vector<std::pair<void*,std::size_t>> segments(1);
-            segments[0].first  = (void*)(&v[0]);
-            segments[0].second = v.size();
-            tl::bulk local = engine.expose(segments, tl::bulk_mode::write_only);
-            b.on(ep) >> local;
-            std::cout << "Server received bulk: ";
-            for(auto c : v) std::cout << c;
-            std::cout << std::endl;
-        };
-    engine.define("do_rdma",f).disable_response();
+    std::string buffer = "Matthieu";
+    std::vector<std::pair<void*,std::size_t>> segments(1);
+    segments[0].first  = (void*)(&buffer[0]);
+    segments[0].second = buffer.size()+1;
+
+    tl::bulk myBulk = engine.expose(segments, tl::bulk_mode::read_only);
+
+    remote_do_rdma.on(server_endpoint)(myBulk);
 }
