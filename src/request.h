69f30568cde749ef0a5166ff93c8276fc2cc25e3
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-05-24 13:25:43 -0700

    import headers

--- src/request.h
+++ src/request.h
@@ -3,14 +3,14 @@
 #include <string>
 
 
-class scan_req {
+class scan_request {
     private:
         std::vector<std::string> proj_cols;
         std::string filter_expr;
 
     public:
 
-        scan_req(std::vector<std::string> proj_cols, std::string filter_expr)
+        scan_request(std::vector<std::string> proj_cols, std::string filter_expr)
         : proj_cols(proj_cols), filter_expr(filter_expr) {}
 
         template<typename A>
