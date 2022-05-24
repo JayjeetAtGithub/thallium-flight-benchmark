4628fb36e157325b7de80f062c79ad64e7af773c
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-05-24 13:29:38 -0700

    import headers

--- src/request.h
+++ src/request.h
@@ -7,11 +7,10 @@
 
 
 class scan_request {
-    private:
+    public:
         std::vector<std::string> proj_cols;
         std::string filter_expr;
 
-    public:
         scan_request(std::vector<std::string> proj_cols, std::string filter_expr)
         : proj_cols(proj_cols), filter_expr(filter_expr) {}
 
