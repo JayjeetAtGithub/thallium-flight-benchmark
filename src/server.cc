7563da6f6e4ead88255af53d1c66b6f6a2eafef0
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-05-26 10:58:36 -0700

    add method to scan a parquet file on the server side (#2)

--- src/server.cc
+++ src/server.cc
@@ -1,12 +1,52 @@
 #include <iostream>
 
-#include <thallium.hpp>
+#include <arrow/api.h>
+#include <arrow/compute/exec/expression.h>
+#include <arrow/dataset/api.h>
+#include <arrow/filesystem/api.h>
+#include <arrow/io/api.h>
+#include <arrow/util/checked_cast.h>
+#include <arrow/util/iterator.h>
+#include <parquet/arrow/reader.h>
+#include <parquet/arrow/writer.h>
+#include "thallium.hpp"
 
 #include "request.h"
 
 
 namespace tl = thallium;
 
+
+arrow::Result<std::shared_ptr<arrow::Table>> Scan() {
+    std::shared_ptr<arrow::fs::LocalFileSystem> fs =
+        std::make_shared<arrow::fs::LocalFileSystem>();
+
+    arrow::fs::FileSelector selector;
+    selector.base_dir = "/mnt/cephfs/dataset";
+    selector.recursive = true;
+
+    ARROW_ASSIGN_OR_RAISE(std::vector<arrow::fs::FileInfo> file_infos,
+        fs->GetFileInfo(selector));
+
+    std::shared_ptr<arrow::dataset::ParquetFileFormat> format =
+        std::make_shared<arrow::dataset::ParquetFileFormat>();
+
+    arrow::dataset::FileSystemFactoryOptions options;
+    ARROW_ASSIGN_OR_RAISE(
+        std::shared_ptr<arrow::dataset::DatasetFactory> dataset_factory,
+        arrow::dataset::FileSystemDatasetFactory::Make(fs, selector, format, options));
+
+    ARROW_ASSIGN_OR_RAISE(std::shared_ptr<arrow::dataset::Dataset> dataset,
+        dataset_factory->Finish());
+
+    arrow::dataset::ScannerBuilder scanner_builder(dataset);
+    ARROW_RETURN_NOT_OK(scanner_builder.UseThreads(true));
+    ARROW_ASSIGN_OR_RAISE(std::shared_ptr<arrow::dataset::Scanner> scanner,
+                        scanner_builder.Finish());
+
+    ARROW_ASSIGN_OR_RAISE(std::shared_ptr<arrow::Table> table, scanner->ToTable());
+    return table;
+}
 
 int main(int argc, char** argv) {
 
