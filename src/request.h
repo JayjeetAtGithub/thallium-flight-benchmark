b639b1f0d5b6265ca6f3cdebd646c139b657c86d
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-05-26 09:50:48 -0700

    updates

--- src/request.h
+++ src/request.h
@@ -11,14 +11,22 @@ class scan_request {
         char *filter_buffer;
         size_t filter_buffer_size;
 
+        char *projection_buffer;
+        size_t projection_buffer_size;
+
         scan_request() {}
-        scan_request(char* filter_buffer, size_t filter_buffer_size)
-        : filter_buffer(filter_buffer), filter_buffer_size(filter_buffer_size) {}
+        scan_request(
+            char* filter_buffer, size_t filter_buffer_size, char* projection_buffer, size_t projection_buffer_size) {}
+        : filter_buffer(filter_buffer), filter_buffer_size(filter_buffer_size), 
+          projection_buffer(projection_buffer), projection_buffer_size(projection_buffer_size) {}
 
         template<typename A>
         void save(A& ar) const {
             ar & filter_buffer_size;
             ar.write(filter_buffer, filter_buffer_size);
+
+            ar & projection_buffer_size;
+            ar.write(projection_buffer, projection_buffer_size);
         }
 
         template<typename A>
@@ -26,5 +34,9 @@ class scan_request {
             ar & filter_buffer_size;
             filter_buffer = new char[filter_buffer_size];
             ar.read(filter_buffer, filter_buffer_size);
+
+            ar & projection_buffer_size;
+            projection_buffer = new char[projection_buffer_size];
+            ar.read(projection_buffer, projection_buffer_size);
         }
 };
