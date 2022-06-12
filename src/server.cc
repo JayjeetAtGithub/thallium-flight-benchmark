2653221c95fb0b667b55245ec2cd6beda92e59e3
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-06-11 23:07:47 -0700

    commit

--- src/server.cc
+++ src/server.cc
@@ -76,7 +76,7 @@ int main(int argc, char** argv) {
             std::shared_ptr<ScanResultConsumer> consumer = Scan(exec_context).ValueOrDie();
             std::shared_ptr<arrow::RecordBatchReader> reader = consumer->reader;
             auto t = reader->ToTable().ValueOrDie();
-            std::cout << "Table: " << t->ToString() << std::endl;
+            std::cout << "Table: " << t->num_rows() << std::endl;
 
             std::string uuid = generate_uuid();
             reader_map[uuid] = reader;
