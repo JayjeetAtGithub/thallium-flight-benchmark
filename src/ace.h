dd099504e9fb4b674f54646ec6663dc042bee7de
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-06-11 23:17:34 -0700

    commit

--- src/ace.h
+++ src/ace.h
@@ -12,29 +12,17 @@ class ScanResultConsumer {
 };
 
 arrow::Result<std::shared_ptr<ScanResultConsumer>> Scan(cp::ExecContext& exec_context) {
-    // instantiate an exec plan
     ARROW_ASSIGN_OR_RAISE(std::shared_ptr<cp::ExecPlan> plan,
                             cp::ExecPlan::Make(&exec_context));
 
-    // get a dummy dataset
     ARROW_ASSIGN_OR_RAISE(std::shared_ptr<arrow::dataset::Dataset> dataset, GetDataset());
 
-    // create an empty projection
-    auto options = std::make_shared<arrow::dataset::ScanOptions>();
+    ARROW_ASSIGN_OR_RAISE(auto scanner_builder, dataset->NewScan());
 
-    // construct the scan node
-    cp::ExecNode* scan;
-    auto scan_node_options = arrow::dataset::ScanNodeOptions{dataset, options};
-    ARROW_ASSIGN_OR_RAISE(scan,
-                            cp::MakeExecNode("scan", plan.get(), {}, scan_node_options));
+    ARROW_ASSIGN_OR_RAISE(auto scanner, scanner_builder->Finish());
 
-    arrow::AsyncGenerator<arrow::util::optional<cp::ExecBatch>> sink_gen;
-    ARROW_RETURN_NOT_OK(
-        cp::MakeExecNode("sink", plan.get(), {scan}, cp::SinkNodeOptions{&sink_gen}));
+    ARROW_ASSIGN_OR_RAISE(auto reader, scanner->ToRecordBatchReader());
 
-    std::shared_ptr<arrow::RecordBatchReader> sink_reader =
-        cp::MakeGeneratorReader(dataset->schema(), std::move(sink_gen), exec_context.memory_pool());
-
-    auto consumer = std::make_shared<ScanResultConsumer>(sink_reader, plan);
+    auto consumer = std::make_shared<ScanResultConsumer>(reader, plan);
     return consumer;
 }
