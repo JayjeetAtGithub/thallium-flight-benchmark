e4e03f6c04f47eb377aad0e35013277c4ad4ca6b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-06-12 10:41:25 -0700

    commit

--- src/client.cc
+++ src/client.cc
@@ -100,6 +100,6 @@ int main(int argc, char** argv) {
     std::string uuid = scan.on(server_endpoint)(req);
     std::cout << "Scan success: Got an UUID: " << uuid << std::endl;
 
-    int e;
-    while ((e = get_next_batch.on(server_endpoint)(uuid)) == 0);
+    int64_t e;
+    while ((e = get_next_batch.on(server_endpoint)(uuid)) != -1);
 }
--- src/server.cc
+++ src/server.cc
@@ -108,9 +108,9 @@ int main(int argc, char** argv) {
 
                 tl::bulk arrow_bulk = engine.expose(segments, tl::bulk_mode::read_only);
                 do_rdma.on(req.get_endpoint())(num_rows, num_cols, types, data_buff_sizes, offset_buff_sizes, arrow_bulk);
-                return req.respond(0);
+                return req.respond(num_rows);
             } else {
-                return req.respond(1);
+                return req.respond(-1);
             }
         };
     
