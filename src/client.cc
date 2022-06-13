11506e7da0c2648ff92f86b4cd3287153e52dac1
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-06-12 18:49:42 -0700

    commit

--- src/client.cc
+++ src/client.cc
@@ -107,7 +107,6 @@ int main(int argc, char** argv) {
     std::string uuid = scan.on(server_endpoint)(req);
     std::cout << "Scan success: Got an UUID: " << uuid << std::endl;
 
-
     std::shared_ptr<arrow::RecordBatch> batch;
     std::cout << batch->ToString();
     while ((batch = GetNextBatch(engine, server_endpoint, uuid).ValueOrDie()) != nullptr) {
