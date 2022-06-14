a1f49bb22399951086d98cefd4e06e44bbc7cc56
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-06-14 12:39:17 -0700

    commit-1

--- src/ace.h
+++ src/ace.h
@@ -14,10 +14,10 @@ class ScanResultConsumer {
 arrow::Result<std::shared_ptr<ScanResultConsumer>> Scan(cp::ExecContext& exec_context, ScanReqRPCStub& stub) {
     ARROW_ASSIGN_OR_RAISE(auto filter,
                             arrow::compute::Deserialize(std::make_shared<arrow::Buffer>(
-                            stub->filter_buffer, stub->filter_buffer_size)));
+                            stub.filter_buffer, stub.filter_buffer_size)));
 
     arrow::ipc::DictionaryMemo empty_memo;
-    arrow::io::BufferReader schema_reader(stub->projection_buffer, stub->projection_buffer_size);
+    arrow::io::BufferReader schema_reader(stub.projection_buffer, stub.projection_buffer_size);
     ARROW_ASSIGN_OR_RAISE(auto schema, arrow::ipc::ReadSchema(&schema_reader, &empty_memo));
 
     ARROW_ASSIGN_OR_RAISE(std::shared_ptr<cp::ExecPlan> plan,
@@ -26,9 +26,8 @@ arrow::Result<std::shared_ptr<ScanResultConsumer>> Scan(cp::ExecContext& exec_co
     ARROW_ASSIGN_OR_RAISE(std::shared_ptr<arrow::dataset::Dataset> dataset, GetDataset());
 
     ARROW_ASSIGN_OR_RAISE(auto scanner_builder, dataset->NewScan());
-    ARROW_RETURN_NOT_OK(scanner_builder->Project(schema));
+    ARROW_RETURN_NOT_OK(scanner_builder->Project(schema->field_names()));
     ARROW_RETURN_NOT_OK(scanner_builder->Filter(filter));
-
     ARROW_ASSIGN_OR_RAISE(auto scanner, scanner_builder->Finish());
 
     ARROW_ASSIGN_OR_RAISE(auto reader, scanner->ToRecordBatchReader());
