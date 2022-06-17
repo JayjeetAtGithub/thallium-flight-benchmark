b6a0559d39a48bb4ba62171464363298c530240a
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-06-16 20:21:45 -0700

    Add dataset deploy script

---
+++ deploy_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ex
+
+mkdir -p /mnt/cephfs/dataset
+wget https://skyhook-ucsc.s3.us-west-1.amazonaws.com/16MB.uncompressed.parquet
+for i in {1..100}; do
+  cp 16MB.uncompressed.parquet /mnt/cephfs/dataset/16MB.uncompressed.parquet.$i
+done
