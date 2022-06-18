1f5dfbdcf19a10234b656a0de2487e21d8b39c31
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-06-18 12:48:02 -0700

    use larger batch size

--- src/client.cc
+++ src/client.cc
@@ -109,7 +109,7 @@ arrow::Result<std::shared_ptr<arrow::RecordBatch>> GetNextBatch(ConnCtx &conn_ct
 
 arrow::Status Main(char **argv) {
     auto filter = 
-        cp::greater(cp::field_ref("total_amount"), cp::literal(-200));
+        cp::greater(cp::field_ref("total_amount"), cp::literal(-80));
     
     auto schema = arrow::schema({arrow::field("passenger_count", arrow::int64()),
                                  arrow::field("fare_amount", arrow::float64())});
