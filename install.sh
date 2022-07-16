24f1066ecf27c0e560339c3435875ca9e315b4ef
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-07-16 10:51:31 -0700

    Build libfabric plugins as loadable libraires

--- install.sh
+++ install.sh
@@ -3,7 +3,14 @@ set -ex
 
 # installing some dependencies
 apt update
-apt install -y uuid-dev libjson-c-dev libpmemobj-cpp-dev gcc-multilib g++-multilib
+apt install -y uuid-dev \
+               libjson-c-dev \
+               libpmemobj-cpp-dev \
+               gcc-multilib \
+               g++-multilib \
+               ibverbs-utils \
+               librdmacm-dev \
+               libfabric-bin
 
 # create the working dir
 rm -rf $HOME/mochi-tools
@@ -15,7 +22,7 @@ echo "building libfabric"
 git clone https://github.com/ofiwg/libfabric
 cd libfabric
 ./autogen.sh
-./configure --prefix=$HOME/mochi/install --enable-tcp=yes --enable-udp=yes --enable-sockets=yes --enable-verbs=yes --enable-rxm=yes
+./configure --prefix=$HOME/mochi/install --enable-tcp=dl --enable-udp=dl --enable-sockets=dl --enable-verbs=dl --enable-rxm=dl
 make -j 32
 make install
 cd ../
@@ -27,7 +34,7 @@ cd mercury
 git submodule update --init
 mkdir build
 cd build
-cmake -DMERCURY_USE_SELF_FORWARD:BOOL=ON \
+PKG_CONFIG_PATH=$HOME/mochi/install/lib/pkgconfig cmake -DMERCURY_USE_SELF_FORWARD:BOOL=ON \
  -DBUILD_TESTING:BOOL=ON -DMERCURY_USE_BOOST_PP:BOOL=ON \
  -DCMAKE_INSTALL_PREFIX=$HOME/mochi/install \
  -DBUILD_SHARED_LIBS:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Debug ..
@@ -85,14 +92,16 @@ make install
 cd ../..
 
 # build cereal
+echo "building cereal"
 git clone https://github.com/USCiLab/cereal
 cd cereal
-cmake .
+PKG_CONFIG_PATH=$HOME/mochi/install/lib/pkgconfig cmake -DCMAKE_INSTALL_PREFIX=$HOME/mochi/install .
 make -j32
 make install
 cd ../
 
 # build thallium
+echo "building thallium"
 git clone https://github.com/mochi-hpc/mochi-thallium
 cd mochi-thallium
 PKG_CONFIG_PATH=$HOME/mochi/install/lib/pkgconfig cmake -DCMAKE_INSTALL_PREFIX=$HOME/mochi/install .
