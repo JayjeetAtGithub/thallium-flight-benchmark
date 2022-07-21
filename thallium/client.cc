14c2da40bf0390cfee2cdc49f6f3e57e58613fee
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-07-21 13:43:48 -0700

    add the finalizer to engine (#16)

--- thallium/client.cc
+++ thallium/client.cc
@@ -149,7 +149,7 @@ arrow::Status Main(char **argv) {
         }
     }
     std::cout << "Read " << total_rows << " rows" << std::endl;
-    exit(0);
+    conn_ctx.engine.finalize();
     return arrow::Status::OK();
 }
 
