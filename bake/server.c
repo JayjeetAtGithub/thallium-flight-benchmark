28dc81a52e2e58b5e226fdf8c46defa114ecaf16
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-07-26 10:39:26 -0700

    add margo code

--- bake/server.c
+++ bake/server.c
@@ -1,41 +1,175 @@
-#include <bake-client.h>
+#include <stdio.h>
+#include <assert.h>
+#include <unistd.h>
+#include <mercury.h>
+#include <abt.h>
+#include <margo.h>
 
-int main(int argc, char **argv) {
-    char *svr_addr_str;
-    hg_addr_t svr_addr;
-    bake_client_t bcl;
+#include "bake-client.h"
+
+static char* read_input_file(const char* filename);
+
+int main(int argc, char* argv[])
+{
+    int                    i;
+    char                   cli_addr_prefix[64] = {0};
+    char*                  bake_svr_addr_str;
+    margo_instance_id      mid;
+    hg_addr_t              svr_addr;
+    uint8_t                mplex_id;
+    bake_client_t          bcl;
     bake_provider_handle_t bph;
-    uint8_t mplex_id;
-    uint32_t target_number;
-    bake_region_id_t rid;
+    uint64_t               num_targets;
+    bake_target_id_t       bti;
+    bake_region_id_t       the_rid;
+    char*                  test_str = NULL;
+    char*                  buf;
+    uint64_t               buf_size;
+    hg_return_t            hret;
+    int                    ret;
 
-    margo_instance_id mid = margo_init("tcp", MARGO_SERVER_MODE, 0, -1);
-    margo_addr_lookup(mid, svr_addr_str, &svr_addr);
+    if (argc != 4) {
+        fprintf(stderr,
+                "Usage: create-write-persist-test <input file> <bake server "
+                "addr> <mplex id>\n");
+        fprintf(
+            stderr,
+            "  Example: ./create-write-persist-test tcp://localhost:1234 1\n");
+        return (-1);
+    }
+    char* input_file  = argv[1];
+    bake_svr_addr_str = argv[2];
+    mplex_id          = atoi(argv[3]);
 
-    bake_client_init(mid, &bcl);
-	/* Creates the provider handle */
-	bake_provider_handle_create(bcl, svr_addr, mplex_id, &bph);
-	/* Asks the provider for up to target_number target ids */
-	uint32_t num_targets = 0;
-	bti = calloc(num_targets, sizeof(*bti));
-	bake_probe(bph, target_number, bti, &num_targets);
-	if(num_targets < target_number) {
-		fprintf(stderr, "Error: provider has only %d storage targets\n", num_targets);
-	}
-	/* Create a region */
-	size_t size = ...; // size of the region to create
-	bake_create(bph, bti[target_number-1], size, &rid);
-	/* Write data into the region at offset 0 */
-	char* buf = ...;
-	bake_write(bph, rid, 0, buf, size);
-	/* Make all modifications persistent */
-	bake_persist(bph, rid);
-	/* Release provider handle */
-	bake_provider_handle_release(bph);
-	/* Release BAKE client */
-	bake_client_finalize(bcl);
-	/* Cleanup Margo resources */
-	margo_addr_free(mid, svr_addr);
-	margo_finalize(mid);
-	return 0;
+    test_str = read_input_file(input_file);
+
+    /* initialize Margo using the transport portion of the server
+     * address (i.e., the part before the first : character if present)
+     */
+    for (i = 0; (i < 63 && bake_svr_addr_str[i] != '\0'
+                 && bake_svr_addr_str[i] != ':');
+         i++)
+        cli_addr_prefix[i] = bake_svr_addr_str[i];
+
+    /* start margo */
+    mid = margo_init(cli_addr_prefix, MARGO_SERVER_MODE, 0, 0);
+    if (mid == MARGO_INSTANCE_NULL) {
+        fprintf(stderr, "Error: margo_init()\n");
+        return (-1);
+    }
+
+    ret = bake_client_init(mid, &bcl);
+    if (ret != 0) {
+        bake_perror("Error: bake_client_init()", ret);
+        margo_finalize(mid);
+        return -1;
+    }
+
+    /* look up the BAKE server address */
+    hret = margo_addr_lookup(mid, bake_svr_addr_str, &svr_addr);
+    if (hret != HG_SUCCESS) {
+        bake_perror("Error: margo_addr_lookup()", ret);
+        bake_client_finalize(bcl);
+        margo_finalize(mid);
+        return (-1);
+    }
+
+    /* create a BAKE provider handle */
+    ret = bake_provider_handle_create(bcl, svr_addr, mplex_id, &bph);
+    if (ret != 0) {
+        bake_perror("Error: bake_provider_handle_create()", ret);
+        margo_addr_free(mid, svr_addr);
+        bake_client_finalize(bcl);
+        margo_finalize(mid);
+        return (-1);
+    }
+    bake_provider_handle_set_eager_limit(bph, 0);
+
+    /* obtain info on the server's BAKE target */
+    ret = bake_probe(bph, 1, &bti, &num_targets);
+    if (ret != 0) {
+        bake_perror("Error: bake_probe()", ret);
+        bake_provider_handle_release(bph);
+        margo_addr_free(mid, svr_addr);
+        bake_client_finalize(bcl);
+        margo_finalize(mid);
+        return (-1);
+    }
+
+    /**** write phase ****/
+    buf_size = strlen(test_str) + 1;
+
+    ret = bake_create_write_persist(bph, bti, test_str, buf_size, &the_rid);
+    if (ret != 0) {
+        bake_perror("Error: bake_create_write_persist()", ret);
+        bake_provider_handle_release(bph);
+        margo_addr_free(mid, svr_addr);
+        bake_client_finalize(bcl);
+        margo_finalize(mid);
+        return (-1);
+    }
+
+    /**** read-back phase ****/
+
+    buf = malloc(buf_size);
+    memset(buf, 0, buf_size);
+
+    uint64_t bytes_read;
+    ret = bake_read(bph, bti, the_rid, 0, buf, buf_size, &bytes_read);
+    if (ret != 0) {
+        bake_perror("Error: bake_read()", ret);
+        free(buf);
+        bake_provider_handle_release(bph);
+        margo_addr_free(mid, svr_addr);
+        bake_client_finalize(bcl);
+        margo_finalize(mid);
+        return (-1);
+    }
+
+    /* check to make sure we get back the string we expect */
+    if (strcmp(buf, test_str) != 0) {
+        fprintf(stderr,
+                "Error: unexpected buffer contents returned from BAKE\n");
+        free(buf);
+        bake_provider_handle_release(bph);
+        margo_addr_free(mid, svr_addr);
+        bake_client_finalize(bcl);
+        margo_finalize(mid);
+        return (-1);
+    }
+
+    /* shutdown the server */
+    ret = bake_shutdown_service(bcl, svr_addr);
+
+    /**** cleanup ****/
+
+    free(buf);
+    free(test_str);
+    bake_provider_handle_release(bph);
+    margo_addr_free(mid, svr_addr);
+    bake_client_finalize(bcl);
+    margo_finalize(mid);
+    return (ret);
+}
+
+static char* read_input_file(const char* filename)
+{
+    size_t ret;
+    FILE*  fp = fopen(filename, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Could not open %s\n", filename);
+        exit(-1);
+    }
+    fseek(fp, 0, SEEK_END);
+    size_t sz = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    char* buf = calloc(1, sz + 1);
+    ret       = fread(buf, 1, sz, fp);
+    if (ret != sz && ferror(fp)) {
+        free(buf);
+        perror("read_input_file");
+        buf = NULL;
+    }
+    fclose(fp);
+    return buf;
 }
