5e75e4284d53f90e0a160172e6da520dfcfdca2c
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-07-27 15:43:32 -0700

    updates

--- bake/server.cc
+++ bake/server.cc
@@ -31,7 +31,7 @@ int main(int argc, char* argv[]) {
     uint64_t config_size = strlen(config) + 1;
     bk::provider *p = bk::provider::create(
         mid, 0, ABT_POOL_NULL, std::string(config, config_size), ABT_IO_INSTANCE_NULL, NULL, NULL);
-    // bk::target target = p->attach_target("/mnt/cephfs/bake.dat");
+    bk::target target = p->attach_target("/mnt/cephfs/bake.dat");
 
     std::cout << "successfully setup provider";
     std::string cfg = p->get_config();
@@ -40,11 +40,10 @@ int main(int argc, char* argv[]) {
     bk::client bcl(mid);
     bk::provider_handle bph(bcl, svr_addr, 0);
     bph.set_eager_limit(0);
-    bk::target tid = bcl.probe(bph, 1)[0];
 
-    // // write phase
-    // uint64_t buf_size = strlen(test_str) + 1;
-    // bk::region rid = bcl.create_write_persist(bph, tid, test_str, buf_size);
+    // write phase
+    uint64_t buf_size = strlen(test_str) + 1;
+    bk::region rid = bcl.create_write_persist(bph, tid, test_str, buf_size);
 
     // // read-back phase
     // void *buf = (void*)malloc(buf_size);
