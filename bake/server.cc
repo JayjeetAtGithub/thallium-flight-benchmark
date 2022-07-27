5c437de5f190fbac3add326d2d373d730ade764a
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-07-27 14:51:23 -0700

    updates

--- bake/server.cc
+++ bake/server.cc
@@ -25,13 +25,12 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    std::string config_filename = "bake/config.json";
-    char *config = read_input_file(config_filename.c_str());
+    char *config = read_input_file("bake/config.json");
 
     // setup provider
     bk::provider *p = bk::provider::create(
         mid, 0, ABT_POOL_NULL, std::string(config, strlen(config)+1), ABT_IO_INSTANCE_NULL, NULL, NULL);
-    bk::target target = p->attach_target("/mnt/cephfs/bake.dat");
+    // bk::target target = p->attach_target("/mnt/cephfs/bake.dat");
 
     bk::client bcl(mid);
     bk::provider_handle bph(bcl, svr_addr, 1);
