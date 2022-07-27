0a32ce6a1127e0dd2b59c8a77837dcfe5f27356b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-07-27 10:13:59 -0700

    updates

--- bake/server.cc
+++ bake/server.cc
@@ -93,7 +93,7 @@ int main(int argc, char* argv[]) {
         return (-1);
     }
 
-    std::cout << buf << "\n";
+    std::cout << static_cast<const void*>(buf) << "\n";
 
     // /* get a raw pointer to the data */
     // void *ptr;
