bfdc0610a1bc7bce4f48954e8a1e2f77457155f3
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-07-27 14:51:47 -0700

    updates

--- bake/server.cc
+++ bake/server.cc
@@ -33,7 +33,7 @@ int main(int argc, char* argv[]) {
     // bk::target target = p->attach_target("/mnt/cephfs/bake.dat");
 
     bk::client bcl(mid);
-    bk::provider_handle bph(bcl, svr_addr, 1);
+    bk::provider_handle bph(bcl, svr_addr, 0);
     bph.set_eager_limit(0);
     bk::target tid = bcl.probe(bph, 1)[0];
 
