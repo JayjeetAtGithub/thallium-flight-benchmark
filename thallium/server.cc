5e9e79c6a8f1279688716d53ae48dbd139c3ad34
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-07-29 20:56:23 -0700

    debug

--- thallium/server.cc
+++ thallium/server.cc
@@ -88,8 +88,10 @@ int main(int argc, char** argv) {
             std::cout << "Scanning region with rid: " << std::string(rid) << std::endl;
 
             uint8_t *ptr = (uint8_t*)bcl.get_data(bph, tid, rid);
-
+            
+            std::cout << "about to scan\n";
             std::shared_ptr<arrow::RecordBatchReader> reader = Scan(stub, ptr).ValueOrDie();
+            std::cout << "scanned\n";
             std::string uuid = boost::uuids::to_string(boost::uuids::random_generator()());
             reader_map[uuid] = reader;
             
