36d31989ffb9c07ac7c28409d005a91410ce748a
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-08-09 10:33:21 +0530

    commits

--- thallium/server.cc
+++ thallium/server.cc
@@ -62,7 +62,7 @@ static char* read_input_file(const char* filename) {
 
 int main(int argc, char** argv) {
 
-    if (argc < 3) {
+    if (argc < 2) {
         std::cout << "./ts <mode>\n";
         std::cout << "mode: \n\n 1: in-memory\n2: ext4\n3: bake\n";
         exit(0);
