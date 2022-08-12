478b0a0d52ed0b210ae994f9e4542cde8a1669c7
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-08-12 11:52:49 +0530

    updates

--- bake_writer.sh
+++ bake_writer.sh
@@ -2,5 +2,5 @@
 set -ex
 
 for i in {1..3000}; do
-    ./bin/bake_writer $(pwd)/yellow_tripdata_2022-01.parquet.${i}
+    ./bin/bake_writer $(pwd)/yellow_tripdata_2022-01.parquet $(pwd)/yellow_tripdata_2022-01.parquet.${i}
 done
