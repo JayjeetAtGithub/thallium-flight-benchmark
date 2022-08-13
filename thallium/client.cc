3068f289aea9075bc932ca05066ee08c9fab921c
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-08-13 07:16:39 +0530

    updates

--- thallium/client.cc
+++ thallium/client.cc
@@ -165,7 +165,7 @@ arrow::Status Main(char **argv) {
         arrow::field("total_amount", arrow::float64())
     });
 
-    projection_schema = dataset_schema;
+    // projection_schema = dataset_schema;
 
     // scan
     ConnCtx conn_ctx = Init(uri);
