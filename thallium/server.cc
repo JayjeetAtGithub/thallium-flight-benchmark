94d45d828dac655815d62b78428315a359cdc630
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-08-19 15:25:51 +0530

    create argobots pool

--- thallium/server.cc
+++ thallium/server.cc
@@ -127,7 +127,7 @@ int main(int argc, char** argv) {
 
     // create a new execution stream
     ABT_xstream xstream;
-    ABT_xstream_create(ABT_SCHED_NULL, &xstream);
+    ABT_xstream_self(&xstream);
 
     std::function<void(const tl::request&, const ScanReqRPCStub&)> scan = 
         [&mid, &svr_addr, &bp, &bcl, &bph, &tid, &db, &mode, &xstream](const tl::request &req, const ScanReqRPCStub& stub) {
