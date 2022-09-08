6c8f9e6000306661296e9035ec528897130731e2
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-09-08 20:46:37 +0530

    debug

--- thallium/server.cc
+++ thallium/server.cc
@@ -116,9 +116,9 @@ class concurrent_queue {
         }
 
         void wait_and_pop(std::shared_ptr<arrow::RecordBatch> &batch) {
-            std::cout << "wait and pop " << is_alive() << std::endl;
+            std::cout << "wait and pop " << batch_queue.empty() << " , " << is_alive() << std::endl;
             std::unique_lock<tl::mutex> lock(m);
-            while (batch_queue.empty()) {
+            while (batch_queue.empty() && is_alive()) {
                 cv.wait(lock);
             }
             if (!batch_queue.empty()) {
