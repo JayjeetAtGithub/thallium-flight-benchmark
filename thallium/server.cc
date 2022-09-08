f760f8bc97cec5eed8e6d27830ee7c93f4081b23
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-09-08 16:55:03 +0530

    Clean up the optimized code (#31)

--- thallium/server.cc
+++ thallium/server.cc
@@ -92,11 +92,13 @@ class concurrent_queue {
 
         void wait_and_pop(std::shared_ptr<arrow::RecordBatch> &batch) {
             std::unique_lock<tl::mutex> lock(m);
-            while (batch_queue.empty()) {
+            while (batch_queue.empty() && is_alive()) {
                 cv.wait(lock);
             }
-            batch = batch_queue.front();
-            batch_queue.pop_front();
+            if (!batch_queue.empty()) {
+                batch = batch_queue.front();
+                batch_queue.pop_front();
+            }
         }
 };
 
@@ -197,15 +199,8 @@ int main(int argc, char** argv) {
     std::function<void(const tl::request&)> get_next_batch = 
         [&mid, &svr_addr, &engine, &do_rdma, &reader_map, &total_rows_written](const tl::request &req) {
             std::shared_ptr<arrow::RecordBatch> batch = nullptr;
+            cq.wait_and_pop(batch);
             
-            if (cq.is_alive()) {
-                cq.wait_and_pop(batch);
-            } else {
-                if (!cq.empty()) {
-                    cq.wait_and_pop(batch);
-                }
-            }
-
             if (batch) {
 
                 std::vector<int64_t> data_buff_sizes;
