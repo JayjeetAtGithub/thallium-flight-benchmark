4e8528f7448be2e6810910007f7a02224855fd42
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-09-08 18:10:18 +0530

    debug

--- thallium/server.cc
+++ thallium/server.cc
@@ -126,7 +126,7 @@ class concurrent_queue {
             }
         }
 
-        void wait_and_pop2(std::shared_ptr<arrow::RecordBatch> &batch) {
+        void pop(std::shared_ptr<arrow::RecordBatch> &batch) {
             std::unique_lock<tl::mutex> lock(m);
             while (batch_queue.empty()) {
                 cv.wait(lock);
@@ -241,13 +241,17 @@ int main(int argc, char** argv) {
         };
 
     int64_t total_rows_written = 0;
-    std::function<void(const tl::request&)> get_next_batch = 
-        [&mid, &svr_addr, &engine, &do_rdma, &total_rows_written](const tl::request &req) {
+    std::function<void(const tl::request&, const bool&)> get_next_batch = 
+        [&mid, &svr_addr, &engine, &do_rdma, &total_rows_written](const tl::request &req, const bool &last) {
             std::shared_ptr<arrow::RecordBatch> batch = nullptr;
-            if (!cq.empty()) {
-                cq.wait_and_pop2(batch);
-            }
-            
+            if (last) {
+                if (!cq.empty()) {
+                    cq.pop(batch);
+                }
+            } else {
+                cq.wait_and_pop(batch);
+            }   
+
             if (batch) {                
                 std::vector<int64_t> data_buff_sizes;
                 std::vector<int64_t> offset_buff_sizes;
