9b4ea74c76b0ab13f379d41aa98a597f337c7b48
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-09-08 18:17:14 +0530

    debug

--- thallium/client.cc
+++ thallium/client.cc
@@ -125,7 +125,9 @@ arrow::Result<std::shared_ptr<arrow::RecordBatch>> GetNextBatch(ConnCtx &conn_ct
     conn_ctx.engine.define("do_rdma", f);
     tl::remote_procedure get_next_batch = conn_ctx.engine.define("get_next_batch");
 
-    int e = get_next_batch.on(conn_ctx.endpoint, last)();
+    // int e = get_next_batch.on(conn_ctx.endpoint, last)();
+    int e = get_next_batch.on(last)();
+
     if (e == 0) {
         return batch;
     } else {
