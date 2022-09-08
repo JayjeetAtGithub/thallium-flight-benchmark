0ef2e4944638d40d827b86f5f5f438d04361e7b9
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-09-08 17:43:05 +0530

    debug

--- thallium/server.cc
+++ thallium/server.cc
@@ -244,8 +244,10 @@ int main(int argc, char** argv) {
     std::function<void(const tl::request&)> get_next_batch = 
         [&mid, &svr_addr, &engine, &do_rdma, &total_rows_written](const tl::request &req) {
             std::shared_ptr<arrow::RecordBatch> batch = nullptr;
-            cq.wait_and_pop2(batch);
- 
+            if (!cq.empty()) {
+                cq.wait_and_pop2(batch);
+            }
+            
             if (batch) {                
                 std::vector<int64_t> data_buff_sizes;
                 std::vector<int64_t> offset_buff_sizes;
