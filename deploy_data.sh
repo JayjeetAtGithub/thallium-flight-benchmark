19d6248df0c1a2cc0df7c6d90d66961a1a844ba2
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-09-21 03:08:11 -0700

    Write 200 files instead of 100

--- deploy_data.sh
+++ deploy_data.sh
@@ -3,6 +3,6 @@ set -ex
 
 mkdir -p /mnt/cephfs/dataset
 wget https://skyhook-ucsc.s3.us-west-1.amazonaws.com/16MB.uncompressed.parquet
-for i in {1..100}; do
+for i in {1..200}; do
   cp 16MB.uncompressed.parquet /mnt/cephfs/dataset/16MB.uncompressed.parquet.$i
 done
