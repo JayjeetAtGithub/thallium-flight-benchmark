31ba9eaa3d06dcf1b39994bdafc01fd44ad4c1d7
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-09-25 12:01:35 -0700

    time blocks of code

--- thallium/client.cc
+++ thallium/client.cc
@@ -33,7 +33,7 @@ class MeasureExecutionTime{
       MeasureExecutionTime(const std::string& caller):caller(caller),begin(std::chrono::steady_clock::now()){}
       ~MeasureExecutionTime(){
           const auto duration=std::chrono::steady_clock::now()-begin;
-          std::cout << (double)std::chrono::duration_cast<std::chrono::milliseconds>(duration).count()/1000<<std::endl;
+          std::cout << caller << ": " << (double)std::chrono::duration_cast<std::chrono::milliseconds>(duration).count()/1000 << " s" <<std::endl;
       }
 };
 
--- thallium/server.cc
+++ thallium/server.cc
@@ -39,6 +39,25 @@ namespace bk = bake;
 namespace cp = arrow::compute;
 namespace yk = yokan;
 
+
+class MeasureExecutionTime{
+  private:
+      const std::chrono::steady_clock::time_point begin;
+      const std::string caller;
+  public:
+      MeasureExecutionTime(const std::string& caller):caller(caller),begin(std::chrono::steady_clock::now()){}
+      ~MeasureExecutionTime(){
+          const auto duration=std::chrono::steady_clock::now()-begin;
+          std::cout << caller << ": " << (double)std::chrono::duration_cast<std::chrono::milliseconds>(duration).count()/1000 << " s" <<std::endl;
+      }
+};
+
+
+#ifndef MEASURE_FUNCTION_EXECUTION_TIME
+#define MEASURE_FUNCTION_EXECUTION_TIME const MeasureExecutionTime measureExecutionTime(__FUNCTION__);
+#endif
+
+
 static char* read_input_file(const char* filename) {
     size_t ret;
     FILE*  fp = fopen(filename, "r");
