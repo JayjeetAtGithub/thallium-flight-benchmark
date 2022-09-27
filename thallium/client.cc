ba4f71671f58cbb100449f9286d523439c926c6b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-09-27 09:48:55 -0700

    measure time in milliseconds

--- thallium/client.cc
+++ thallium/client.cc
@@ -33,7 +33,7 @@ class MeasureExecutionTime{
       MeasureExecutionTime(const std::string& caller):caller(caller),begin(std::chrono::steady_clock::now()){}
       ~MeasureExecutionTime(){
           const auto duration=std::chrono::steady_clock::now()-begin;
-          std::cout << caller << ": " << (double)std::chrono::duration_cast<std::chrono::milliseconds>(duration).count()/1000 << " s" <<std::endl;
+          std::cout << caller << ": " << (double)std::chrono::duration_cast<std::chrono::microseconds>(duration).count()/1000 << " ms" <<std::endl;
       }
 };
 
