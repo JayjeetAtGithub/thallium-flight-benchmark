c64ff8a31b0912bc96396a399d7ba7838e8c9763
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-10-21 10:32:11 -0700

    remove the existing softlinks

--- scripts/server.sh
+++ scripts/server.sh
@@ -5,6 +5,7 @@ selectivity=$1
 protocol=$2
 
 export PROJECT_ROOT=$HOME/thallium-flight-benchmark
+rm -rf *.json
 ln -s $PROJECT_ROOT/bake_config.json bake_config.json
 ln -s $PROJECT_ROOT/yokan_config.json yokan_config.json
 $PROJECT_ROOT/bin/ts $selectivity dataset+mem $protocol
