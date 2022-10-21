d58d75fbb14cff9cad29209b32d333fc3e84f6bc
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-10-21 10:31:27 -0700

    soft link the config files to the script dir

--- scripts/server.sh
+++ scripts/server.sh
@@ -5,4 +5,6 @@ selectivity=$1
 protocol=$2
 
 export PROJECT_ROOT=$HOME/thallium-flight-benchmark
+ln -s $PROJECT_ROOT/bake_config.json bake_config.json
+ln -s $PROJECT_ROOT/yokan_config.json yokan_config.json
 $PROJECT_ROOT/bin/ts $selectivity dataset+mem $protocol
