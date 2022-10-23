c7d91d2a6f56e822a43d4ba2a775d8586b50c748
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-10-23 14:39:08 -0700

    download the parquet file in the bake script

--- scripts/bake_writer.sh
+++ scripts/bake_writer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
 
+wget https://skyhook-ucsc.s3.us-west-1.amazonaws.com/16MB.uncompressed.parquet
 for i in {1..200}; do
     ./bin/bake_writer $(pwd)/16MB.uncompressed.parquet /mnt/cephfs/dataset/16MB.uncompressed.parquet.${i}
 done
