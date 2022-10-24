f30dea01a6132d5c818dcb01b929db0f58f69a45
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-10-24 15:53:06 -0700

    profile the server

--- thallium/client.cc
+++ thallium/client.cc
@@ -198,7 +198,7 @@ arrow::Status Main(int argc, char **argv) {
                 total_rows += batch->num_rows();
             }
         }
-        std::cout << "total partwise : " << total_time_partwise << " ms " << std::endl;
+        std::cout << "total_partwise : " << total_time_partwise << " ms " << std::endl;
     } else {
         {
             MeasureExecutionTime m("total");
