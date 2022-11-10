c3fd6f4c0f70fb8d6be1fa9eb7c4e31badaf1819
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-11-09 17:58:44 -0800

    modifications

--- thallium/server.cc
+++ thallium/server.cc
@@ -146,7 +146,7 @@ int main(int argc, char** argv) {
 
     int64_t total_rows_written = 0;
     std::function<void(const tl::request&, const std::string&)> get_next_batch = 
-        [&mid, &svr_addr, &engine, &do_rdma, &reader_map, &total_rows_written](const tl::request &req, const std::string& uuid) {
+        [&mid, &svr_addr, &engine, &do_rdma, &reader_map, &total_rows_written, &arrow_bulk](const tl::request &req, const std::string& uuid) {
             
             std::shared_ptr<arrow::RecordBatchReader> reader = reader_map[uuid];
             std::shared_ptr<arrow::RecordBatch> batch;
