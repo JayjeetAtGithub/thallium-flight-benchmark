7bddb74b08c3aeaff7bfb3388dce7024a912657b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-11-09 18:02:33 -0800

    modifications

--- thallium/server.cc
+++ thallium/server.cc
@@ -178,10 +178,10 @@ int main(int argc, char** argv) {
                         data_size = data_buff->size();
                         offset_size = offset_buff->size();
                         // segments[i*2].first = (void*)data_buff->data();
-                        memcpy(segments[i*2].first, data_buff->data(), data_size);
+                        memcpy(segments[i*2].first, (void*)data_buff->data(), data_size);
                         segments[i*2].second = data_size;
                         // segments[(i*2)+1].first = (void*)offset_buff->data();
-                        memcpy(segments[(i*2)+1].first, offset_buff->data(), offset_size);
+                        memcpy(segments[(i*2)+1].first, (void*)offset_buff->data(), offset_size);
                         segments[(i*2)+1].second = offset_size;
                     } else {
                         std::shared_ptr<arrow::Buffer> data_buff = 
@@ -189,10 +189,10 @@ int main(int argc, char** argv) {
                         data_size = data_buff->size();
                         offset_size = null_buff.size() + 1; 
                         // segments[i*2].first  = (void*)data_buff->data();
-                        memcpy(segments[i*2].first, data_buff->data(), data_size);
+                        memcpy(segments[i*2].first, (void*)data_buff->data(), data_size);
                         segments[i*2].second = data_size;
                         // segments[(i*2)+1].first = (void*)(&null_buff[0]);
-                        memcpy(segments[(i*2)+1].first, &null_buff[0], offset_size);
+                        memcpy(segments[(i*2)+1].first, (void*)(&null_buff[0]), offset_size);
                         segments[(i*2)+1].second = offset_size;
                     }
 
