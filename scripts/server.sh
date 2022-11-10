0c957d77e3a17040242a562543e28ae2a13c4d73
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-11-10 11:10:25 -0800

    copy config file

--- scripts/server.sh
+++ scripts/server.sh
@@ -6,6 +6,6 @@ protocol=$2
 
 export PROJECT_ROOT=$HOME/thallium-flight-benchmark
 rm -rf *.json
-ln -s $PROJECT_ROOT/bake_config.json bake_config.json
-ln -s $PROJECT_ROOT/yokan_config.json yokan_config.json
+cp $PROJECT_ROOT/bake_config.json bake_config.json
+cp $PROJECT_ROOT/yokan_config.json yokan_config.json
 $PROJECT_ROOT/bin/ts $selectivity dataset+mem $protocol
