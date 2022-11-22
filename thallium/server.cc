487d3c0dd5fc4f79f3740dcac6ea56bcc7a2d71b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-11-21 20:01:32 -0800

    make changes

--- thallium/server.cc
+++ thallium/server.cc
@@ -201,7 +201,7 @@ int main(int argc, char** argv) {
                     }
                 }
 
-                segments[0].first = (void*)segment_buffer;
+                // segments[0].first = (void*)segment_buffer;
                 segments[0].second = total_size;
 
                 do_rdma.on(req.get_endpoint())(num_rows, data_offsets, data_sizes, off_offsets, off_sizes, total_size, arrow_bulk);
