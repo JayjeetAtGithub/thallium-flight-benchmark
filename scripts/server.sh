c2f1a6e8fd1c7eb075e52744fe7a3cb7d27381f9
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-01-26 18:31:20 -0800

    Fix the protocol to ofi+verbs

--- scripts/server.sh
+++ scripts/server.sh
@@ -2,10 +2,9 @@
 set -e
 
 selectivity=$1
-protocol=$2
 
 export PROJECT_ROOT=$HOME/thallium-flight-benchmark
 # rm -rf *.json
 # cp $PROJECT_ROOT/bake_config.json bake_config.json
 # cp $PROJECT_ROOT/yokan_config.json yokan_config.json
-$PROJECT_ROOT/bin/ts $selectivity dataset+mem $protocol
+$PROJECT_ROOT/bin/ts $selectivity dataset+mem ofi+verbs
