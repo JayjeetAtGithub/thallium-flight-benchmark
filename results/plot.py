8a072e59a154cf7c7b9c8396610e0476d2265443
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-01-28 08:22:44 -0800

    Add plot script

---
+++ results/plot.py
@@ -0,0 +1,33 @@
+import os
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    selectivity = ["1", "10", "100"]
+    mode = ["client", "server"]
+    benchmark = ["0", "2"]
+
+    for b in benchmark:
+        for s in selectivity:
+            data_dict = {}
+            x = []
+            y = []
+            for m in mode:
+                path = b + "/" + "result_" + m + "_" + s + ".txt"
+                print(path)
+                if os.path.exists(path):
+                    with open(path, "r") as f:
+                        lines = f.readlines()
+                        for line in lines[:-1]:
+                            key = str(line.split(":")[0].strip())
+                            val = float(line.split(":")[1].strip())
+                            if key not in data_dict:
+                                data_dict[key] = []
+                            data_dict[key].append(val)
+            for key in data_dict:
+                data_dict[key] = sum(data_dict[key])
+            print(data_dict)
+            plt.bar(data_dict.keys(), data_dict.values())
+            plt.ylim(0, 12000)
+            plt.savefig(b + "_" + s + ".pdf")
+            plt.cla()
+            plt.clf()
