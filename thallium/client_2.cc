1f682f7eb1acd01a87dc668f396148e8fbfd0355
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-01-27 20:08:22 -0800

    Updates

--- thallium/client_2.cc
+++ thallium/client_2.cc
@@ -211,7 +211,7 @@ arrow::Status Main(int argc, char **argv) {
                 std::string filepath = "/mnt/cephfs/dataset/16MB.uncompressed.parquet." + std::to_string(i);
                 ARROW_ASSIGN_OR_RAISE(auto scan_req, GetScanRequest(filepath, filter, schema, schema));
                 ScanCtx scan_ctx = Scan(conn_ctx, scan_req);
-                while ((batch = GetNextBatch(conn_ctx, scan_ctx).ValueOrDie()) != nullptr) {
+                while ((batch = GetNextBatch(conn_ctx, scan_ctx, 1).ValueOrDie()) != nullptr) {
                     total_rows += batch->num_rows();
                 }
             }
