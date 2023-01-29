114dbaf61bc2960e0f647d3e0af23c169038fe30
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-01-29 08:57:08 -0800

    Add client/server 1 scripts

--- thallium/client_1.cc
+++ thallium/client_1.cc
@@ -92,10 +92,10 @@ std::vector<std::unique_ptr<arrow::Buffer>> data_buffs;
 std::vector<std::unique_ptr<arrow::Buffer>> offset_buffs;
 std::vector<std::pair<void*,std::size_t>> segments;
 
-arrow::Result<std::shared_ptr<arrow::RecordBatch>> GetNextBatch(ConnCtx &conn_ctx, ScanCtx &scan_ctx) {
+arrow::Result<std::shared_ptr<arrow::RecordBatch>> GetNextBatch(ConnCtx &conn_ctx, ScanCtx &scan_ctx, int flag) {
     std::shared_ptr<arrow::RecordBatch> batch;
     std::function<void(const tl::request&, int64_t&, std::vector<int64_t>&, std::vector<int64_t>&, tl::bulk&)> f =
-        [&conn_ctx, &scan_ctx, &batch, &data_buffs, &offset_buffs, &segments](const tl::request& req, int64_t& num_rows, std::vector<int64_t>& data_buff_sizes, std::vector<int64_t>& offset_buff_sizes, tl::bulk& b) {
+        [&conn_ctx, &scan_ctx, &batch, &data_buffs, &offset_buffs, &segments, &flag](const tl::request& req, int64_t& num_rows, std::vector<int64_t>& data_buff_sizes, std::vector<int64_t>& offset_buff_sizes, tl::bulk& b) {
             int num_cols = scan_ctx.schema->num_fields();
             tl::bulk local;
 
@@ -202,7 +202,7 @@ arrow::Status Main(int argc, char **argv) {
         std::shared_ptr<arrow::RecordBatch> batch;
         {
             MEASURE_FUNCTION_EXECUTION_TIME
-            while ((batch = GetNextBatch(conn_ctx, scan_ctx).ValueOrDie()) != nullptr) {
+            while ((batch = GetNextBatch(conn_ctx, scan_ctx, (total_rows == 0)).ValueOrDie()) != nullptr) {
                 total_rows += batch->num_rows();
             }
         }
