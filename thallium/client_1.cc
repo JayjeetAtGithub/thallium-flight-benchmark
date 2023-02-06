71e3fd8a81b8cb96e2c6abc7af1a392d968a6c68
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-02-05 17:48:04 -0800

    Time get_next_batch RPC

--- thallium/client_1.cc
+++ thallium/client_1.cc
@@ -157,8 +157,11 @@ arrow::Result<std::shared_ptr<arrow::RecordBatch>> GetNextBatch(ConnCtx &conn_ct
         };
     conn_ctx.engine.define("do_rdma", f);
     tl::remote_procedure get_next_batch = conn_ctx.engine.define("get_next_batch");
-
-    int e = get_next_batch.on(conn_ctx.endpoint)(scan_ctx.uuid);
+    int e;
+    {
+        MeasureExecutionTime m("get_next_batch");
+        e = get_next_batch.on(conn_ctx.endpoint)(scan_ctx.uuid);
+    }
     if (e == 0) {
         return batch;
     } else {
