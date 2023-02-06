e8f748fff649530007d8bd00e1fc7eaf473cd848
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-02-06 00:10:43 -0800

    Measure execution time

--- thallium/client_1.cc
+++ thallium/client_1.cc
@@ -94,7 +94,7 @@ std::vector<std::pair<void*,std::size_t>> segments;
 tl::bulk local;
 
 arrow::Result<std::shared_ptr<arrow::RecordBatch>> GetNextBatch(ConnCtx &conn_ctx, ScanCtx &scan_ctx, int flag) {
-    // std::shared_ptr<arrow::RecordBatch> batch;
+    std::shared_ptr<arrow::RecordBatch> batch;
     std::function<void(const tl::request&, int64_t&, std::vector<int64_t>&, std::vector<int64_t>&, tl::bulk&)> f =
         [&conn_ctx, &scan_ctx, &segments, &flag, &local](const tl::request& req, int64_t& num_rows, std::vector<int64_t>& data_buff_sizes, std::vector<int64_t>& offset_buff_sizes) {
             // int num_cols = scan_ctx.schema->num_fields();
@@ -162,10 +162,10 @@ arrow::Result<std::shared_ptr<arrow::RecordBatch>> GetNextBatch(ConnCtx &conn_ct
     // int e;
     // {
         // MeasureExecutionTime m("get_next_batch");
-    return get_next_batch.on(conn_ctx.endpoint)(scan_ctx.uuid);
+    get_next_batch.on(conn_ctx.endpoint)(scan_ctx.uuid);
     // }
     // if (e == 0) {
-    //     return batch;
+        return batch;
     // } else {
     //     return nullptr;
     // }
