1352f8bc348d8a6c1eb42871e3e0e62e86d58e86
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-02-17 15:26:16 -0800

    Check data integrity using CRC

--- thallium/client_1.cc
+++ thallium/client_1.cc
@@ -130,6 +130,11 @@ arrow::Result<std::shared_ptr<arrow::RecordBatch>> GetNextBatch(ConnCtx &conn_ct
                 }
             }
 
+            for (int i = 0; i < num_cols; i++) {
+                    segments[i*2].second = data_buff_sizes[i];
+                    segments[(i*2)+1].second = offset_buff_sizes[i];
+                }
+
             std::vector<std::shared_ptr<arrow::Array>> columns;
             if (flag == 1) {
                 std::cout << "Pinning client side buffers" << std::endl;
