e96cff7038c3fba958af8f7b8c165ae600b86bba
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-02-17 16:19:20 -0800

    Check data integrity using CRC

--- thallium/client_1.cc
+++ thallium/client_1.cc
@@ -133,6 +133,10 @@ arrow::Result<std::shared_ptr<arrow::RecordBatch>> GetNextBatch(ConnCtx &conn_ct
 
             // on setting segment sizes here corrupt second time 
 
+            for (int i = 0; i < num_cols; i++) {
+                segments[i*2].second = data_buff_sizes[i];
+                segments[(i*2)+1].second = offset_buff_sizes[i];
+            }
 
             std::vector<std::shared_ptr<arrow::Array>> columns;
             if (flag == 1) {
@@ -149,10 +153,6 @@ arrow::Result<std::shared_ptr<arrow::RecordBatch>> GetNextBatch(ConnCtx &conn_ct
             }
 
 
-            // for (int i = 0; i < num_cols; i++) {
-            //     segments[i*2].second = data_buff_sizes[i];
-            //     segments[(i*2)+1].second = offset_buff_sizes[i];
-            // }
 
             for (int64_t i = 0; i < num_cols; i++) {
                 std::shared_ptr<arrow::DataType> type = scan_ctx.schema->field(i)->type();  
