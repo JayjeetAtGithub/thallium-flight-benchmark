3c4fce58ea6bf9490cbc55e986f79848f7d4660a
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-02-24 22:06:48 -0800

    Use dataset mode

--- thallium/client.cc
+++ thallium/client.cc
@@ -199,6 +199,7 @@ arrow::Status Main(int argc, char **argv) {
             MEASURE_FUNCTION_EXECUTION_TIME
             while ((batch = GetNextBatch(conn_ctx, scan_ctx).ValueOrDie()) != nullptr) {
                 std::cout << batch->ToString() << std::endl;
+                std::cout << "Read " << batch->num_rows() << " rows" << std::endl;
                 total_rows += batch->num_rows();
             }
         }
