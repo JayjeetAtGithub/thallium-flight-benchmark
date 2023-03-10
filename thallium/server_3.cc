16ed49b8b6356747190a1b3b791694d03682e317
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-03-09 20:19:17 -0800

    Fix server3

--- thallium/server_3.cc
+++ thallium/server_3.cc
@@ -1,3 +1,22 @@
+#include <iostream>
+#include <unordered_map>
+#include <fstream>
+
+#include <arrow/api.h>
+#include <arrow/compute/exec/expression.h>
+#include <arrow/dataset/api.h>
+#include <arrow/dataset/plan.h>
+#include <arrow/filesystem/api.h>
+#include <arrow/io/api.h>
+#include <arrow/util/checked_cast.h>
+#include <arrow/util/iterator.h>
+
+#include <arrow/array/array_base.h>
+#include <arrow/array/array_nested.h>
+#include <arrow/array/data.h>
+#include <arrow/array/util.h>
+#include <arrow/testing/random.h>
+#include <arrow/util/key_value_metadata.h>
 
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
