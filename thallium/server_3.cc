8a0cbe8f4923ece02de82d01c5ee5ac5b37ad96d
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-03-10 14:37:47 -0800

    try to use a larger batch

--- thallium/server_3.cc
+++ thallium/server_3.cc
@@ -143,7 +143,7 @@ int main(int argc, char** argv) {
             int32_t total_rows_in_transfer_batch = 0;
             while (total_rows_in_transfer_batch < 131072) {
                 {    
-                    MeasureExecutionTime m("I/O");
+                    // MeasureExecutionTime m("I/O");
                     reader->ReadNext(&batch);
                 }
                 if (batch == nullptr) {
