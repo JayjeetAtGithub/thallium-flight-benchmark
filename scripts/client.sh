cdc4bc8692410ef0a07030c3beef61b5eb60ac0f
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-03-12 22:59:00 -0700

    Add files

--- scripts/client.sh
+++ scripts/client.sh
@@ -5,14 +5,9 @@ binary=$1
 
 export PROJECT_ROOT=$HOME/thallium-flight-benchmark
 
-for i in {1..5};
-do
-
-ssh node1 "sync"
-ssh node1 "echo 3 > /proc/sys/vm/drop_caches"
-ssh node1 "sync"
+sync
+echo 3 > /proc/sys/vm/drop_caches
+sync
 
 uri=$(ssh node1 "cat /tmp/thallium_uri")
 $PROJECT_ROOT/bin/$binary $uri
-
-done
--- scripts/server.sh
+++ scripts/server.sh
@@ -4,5 +4,9 @@ set -e
 selectivity=$1
 binary=$2
 
+sync
+echo 3 > /proc/sys/vm/drop_caches
+sync
+
 export PROJECT_ROOT=$HOME/thallium-flight-benchmark
 $PROJECT_ROOT/bin/$binary $selectivity dataset+mem
