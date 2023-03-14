cafda2dbb3d67fbab0c9f0e1b8119626952beb01
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-03-14 10:47:02 -0700

    Add version 5

--- scripts/client.sh
+++ scripts/client.sh
@@ -9,5 +9,9 @@ sync
 echo 3 > /proc/sys/vm/drop_caches
 sync
 
+ssh node1 "sync"
+ssh node1 "echo 3 > /proc/sys/vm/drop_caches"
+ssh node1 "sync"
+
 uri=$(ssh node1 "cat /tmp/thallium_uri")
 $PROJECT_ROOT/bin/$binary $uri
